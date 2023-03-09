4650c065245a245455da157e36c90228591ee59a
Jonathan DUFOUR <jodufour@student.42.fr>
2023-03-09 11:30:11 +0100

    Update the `s_shell` struct to contain tokens and env as linked list [OK]
    Implement builtins                                                   [TODO]
      cd                                                                 [TODO]
      echo                                                               [TODO]
      env                                                                [TODO]
      exit                                                               [TODO]
      export                                                             [TODO]
      pwd                                                                [TODO]
      unset                                                              [TODO]

--- include/shell.h
+++ include/shell.h
@@ -3,25 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   shell.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 20:36:31 by mcourtoi          #+#    #+#             */
-/*   Updated: 2023/03/08 20:37:51 by mcourtoi         ###   ########.fr       */
+/*   Updated: 2023/03/09 11:17:55 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHELL_H
 # define SHELL_H
 
+# include "list.h"
+
 typedef struct s_shell		t_shell;
 
 struct s_shell
 {
-	char	*line;
-	char	**tokens;
-	char	**env;
-	char	*l_hd;
-	char	*stock_hd;
+	t_token_lst	tokens;
+	t_env_lst	env;
+	char		*line;
+	char		*line_hd;
+	char		*stock_hd;
 };
 
 #endif

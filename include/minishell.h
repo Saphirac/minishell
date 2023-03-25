36dc0e051206c084a9fd8493256314a4c9dcdb50
jodufour <jodufour@student.42.fr>
2023-03-25 15:38:53 +0100

    Remove useless protoypes in `minishell.h` [OK]

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 12:55:28 by mcourtoi          #+#    #+#             */
-/*   Updated: 2023/03/23 23:25:00 by jodufour         ###   ########.fr       */
+/*   Updated: 2023/03/25 15:38:11 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,18 +37,9 @@ extern uint8_t	g_exit_code;
 
 void	prompt(t_shell *const shell)
 		__attribute__((nonnull));
-void	free_tab(char **tab);
 void	handle_signal(int sig);
 void	signal_handle_interactive(void);
 void	signal_handle_non_interactive(void);
-int		count_tokens(char *str);
-int		count_quotes(char *str, int i);
-char	**tokens_tab(t_shell *shell, int i);
-char	**get_commands(t_shell *shell);
-char	*expand_single_quotes(char *token);
-char	*expand_double_quotes(t_shell *shell, char *token);
-char	*search_env(char **env, char *token);
-char	*expand_dollar(char *token, char *tmp);
 
 // Tokens //
 int		ft_is_sep(char c);
@@ -64,18 +55,16 @@ int		ft_if_operator(t_token *tmp, bool *cmd);
 int		ft_if_command(t_token *tmp, bool *cmd);
 
 // Utils //
-void	ft_free(char **tab);
-char	*access_path(char **paths, char *cmd);
-char	*add_path(char *str, char *av1, char c);
-char	*find_apath(char **env);
-char	*raw_curpath(t_env_lst const *const env, char const *const dir)
-		__attribute__((nonnull));
 
 // Builtins //
-int		surprise(void);
 int		canonicalize(char *const curpath)
 		__attribute__((nonnull));
+int		surprise(void);
+
 bool	is_directory(char const *const pathname)
+		__attribute__((nonnull));
+
+char	*raw_curpath(t_env_lst const *const env, char const *const dir)
 		__attribute__((nonnull));
 
 // Errors //
